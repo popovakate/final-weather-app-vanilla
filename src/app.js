7d9e7e5f3792976af79fbd034d1fd18ff913c97e
popovakate <108681165+popovakate@users.noreply.github.com>
2022-08-09 17:45:39 +0300

    added functions

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return "Friday 05:00";
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("temperature");
   let cityElement = document.querySelector("city");
   let descriptionElement = document.querySelector("description");
   let humidityElement = document.querySelector("humidity");
   let windElement = document.querySelector("wind");
+  let dateElement = document.querySelector("date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].decription;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.main.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "ab01f5429d25b879daffdaa00545a954";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
