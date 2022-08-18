0c35d58f9f2c02896f918bc2ca328e5944f07636
popovakate <108681165+popovakate@users.noreply.github.com>
2022-08-18 14:20:19 +0300

    added relevant icon

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("humidity");
   let windElement = document.querySelector("wind");
   let dateElement = document.querySelector("date");
+  let iconElement = document.querySelector("icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -35,7 +36,13 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.main.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
+iconElement.setAttribute("alt", response.data.weather[0].decription);
 let apiKey = "ab01f5429d25b879daffdaa00545a954";
+let city = "Sydney";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
