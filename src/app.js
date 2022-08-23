e273ff0d3619f963d59837c90a6e30b3ce8ef710
popovakate <108681165+popovakate@users.noreply.github.com>
2022-08-23 17:25:38 +0300

    Added daily forecast

--- src/app.js
+++ src/app.js
@@ -21,27 +21,43 @@ function formatedDate() {
   return `${day} ${currentHour}:${currentMinute}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6)
+      forecastHTML =
+        forecastHTML +
+        `
      <div class="col-2">
         <div class="weather-forecast-date">
-          ${day}
+          ${formatDay(forecastDay.dt)}
          </div>
           <img
-          src="http://openweathermap.org/img/wn/04d@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"/>
           <div class="weather-forecats-temperatures">
-           <span class="weather-forecast-max">18 </span>
-            <span class="weather-forecast-min">12</span>
+           <span class="weather-forecast-max">${Math.round(
+             forecastDay.temp.max
+           )}° </span>
+            <span class="weather-forecast-min">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
           </div>
         </div>
   `;
